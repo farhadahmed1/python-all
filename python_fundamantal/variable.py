7b2e1f9c6cc8429927860ad30878f25e81b29706
Farhad Ahmed <farhad.ahmed.aust@gmail.com>
2022-10-03 00:52:09 +0600

    python fundamantel

---
+++ python_fundamantal/variable.py
@@ -0,0 +1,24 @@
+# number type data 
+
+number = 22 
+product_price = 454
+sum = number + product_price 
+print(sum )
+
+# string type data
+
+first_name = "Farhad"
+last_name  = "Ahmed"
+
+full_name = first_name + " " + last_name
+print(full_name)
+
+# user input value 
+#user_first_name = input("user_first_name : "  )
+#user_last_name = input ("user_last_name : " )
+#user_full_name = user_first_name + " " + user_last_name 
+
+user = input("user_first_name :") + " " + input ("user_last_name :" )
+
+print(user)
+
