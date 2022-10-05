a2b9bb9584c222311f2673d7987d64ae71d74a39
Farhad Ahmed <farhad.ahmed.aust@gmail.com>
2022-10-06 02:48:31 +0600

    condition added

---
+++ python_fundamantal/if_else.py
@@ -0,0 +1,16 @@
+product_price = int(input("product_price :"))
+# input data all time string types
+if product_price < 400 :
+    print("sales is product")
+else:
+    print("sorry price is very high")    
+
+exam = "Bank"
+passed= False
+if exam == "BCS":
+    print("you get  marriage")
+if passed == True :
+    print("congrats")
+else:
+    print(" try to next time again")    
+   
--- python_fundamantal/variable.py
+++ python_fundamantal/variable.py
@@ -14,6 +14,7 @@ full_name = first_name + " " + last_name
 print(full_name)
 
 # user input value 
+
 #user_first_name = input("user_first_name : "  )
 #user_last_name = input ("user_last_name : " )
 #user_full_name = user_first_name + " " + user_last_name 
@@ -21,4 +22,20 @@ print(full_name)
 user = input("user_first_name :") + " " + input ("user_last_name :" )
 
 print(user)
+
+# student
+
+student_name ="Arian"
+student_age = 17
+
+print(f"{student_name} is my student .He is {student_age} years old")
+
+# Boolean
+is_hungry = True
+is_poor = False
+
+
+
+
+
 
