d75d4fb8798fdba5d5060d3fac48d2a28c8cd842
Farhad Ahmed <farhad.ahmed.aust@gmail.com>
2022-12-12 20:14:34 +0600

    python problem solved

---
+++ problem_sloveing/Basic_problem/day_one.py
@@ -0,0 +1,19 @@
+# problem 1
+
+# import math
+# number =float(input(" enter a floating number : ")) 
+# ceiling_number = math.ceil(number)
+# floor_number = math.floor(number)
+# print(ceiling_number)
+# print(floor_number)
+
+# problem 2 3 integer inputs from user and
+# outputs absolute values
+
+x, y, z = input("Enter three values: ").split()
+abs_x =abs(int(x))
+abs_y =abs(int(y))
+abs_z =abs(int(z))
+print("absolute num1: ", abs_x)
+print("absolute num2 : ", abs_y)
+print("absolute num3: ", abs_z)
---
+++ python_fundamantal/nested_condition.py
@@ -0,0 +1,16 @@
+# exam = " BCS"
+# passed= False
+# job  = "Bank"
+
+# if exam  == " BCS" and job == "Bank":
+#     print('you get marriage ')
+# else: print(' tor kopaly biye nai Bhai ') 
+
+i= 1 
+number = input(" set a number : " )
+while( i< int (number) ):
+    print(i , end="")
+  
+    if i == 5:
+        break
+    i+=1
